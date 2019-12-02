0f689cb8b1a7658a4ad01a65c6435367a6b6e0fe
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-02 18:54:17 +0530

    Update TestseleTest.java

--- src/main/test/dynamic_new/TestseleTest.java
+++ src/main/test/dynamic_new/TestseleTest.java
@@ -4,6 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 public class TestseleTest {
 	
 	
@@ -27,7 +37,7 @@ public class TestseleTest {
       dropdown.findElement(By.xpath("//option[. = 'Dollar']")).click();
     }
     driver.findElement(By.name("Currency")).click();
-    driver.findElement(By.name("num")).click();
+ 
     driver.findElement(By.name("num")).sendKeys("10");
     driver.findElement(By.name("bookButton")).click();
     driver.findElement(By.cssSelector("input:nth-child(2)")).click();
@@ -37,7 +47,6 @@ public class TestseleTest {
       dropdown.findElement(By.xpath("//option[. = 'Euro']")).click();
     }
     driver.findElement(By.name("Currency")).click();
-    driver.findElement(By.name("num")).click();
     driver.findElement(By.name("num")).sendKeys("10");
     driver.findElement(By.name("bookButton")).click();
     driver.findElement(By.name("Currency")).click();
@@ -48,7 +57,6 @@ public class TestseleTest {
       dropdown.findElement(By.xpath("//option[. = 'Riyal']")).click();
     }
     driver.findElement(By.name("Currency")).click();
-    driver.findElement(By.name("num")).click();
     driver.findElement(By.name("num")).sendKeys("10");
     driver.findElement(By.name("bookButton")).click();
     driver.close();
